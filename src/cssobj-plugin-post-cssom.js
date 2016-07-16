1fa46f2a8cae528ff09af08544b570f04438c07e
1111hui <26634873@qq.com>
2016-07-16 13:34:26 +0800

    ADD: media test enable for old IE

--- src/cssobj-plugin-post-cssom.js
+++ src/cssobj-plugin-post-cssom.js
@@ -5,6 +5,11 @@ function dashify(str) {
     .replace(/([A-Z])/g, function(m){return "-"+m.toLowerCase()})
 }
 
+function sugar(str) {
+  return str
+    .replace(/>=/g, 'min-width:')
+    .replace(/<=/g, 'max-width:')
+}
 
 
 function createDOM (id, option) {
@@ -68,60 +73,134 @@ function cssobj_plugin_post_cssom (option) {
 
   // helper regexp & function
   var reWholeRule = /keyframes|page/
-  var atomGroupRule = function(node) {
-    return !node ? false : reWholeRule.test(node.at) || node.parentRule && reWholeRule.test(node.parentRule.at)
-  }
-  var getOm = function(node) {
+    var atomGroupRule = function(node) {
+      return !node ? false : reWholeRule.test(node.at) || node.parentRule && reWholeRule.test(node.parentRule.at)
+    }
+  var getParent = function(node) {
     var p = node.parentRule
-    return p && ('omGroup' in p) ? p.omGroup : sheet
+    return p && p.omGroup || sheet
+  }
+
+  var validParent = function(node) {
+    return !node.parentRule || node.parentRule.omGroup!==null
+  }
+
+  var removeRule = function(node) {
+    node.omRule && node.omRule.forEach(function(rule) {
+      var parent = rule.parentRule || sheet
+      var rules = parent.cssRules || parent.rules
+      var index = -1
+      for(var i = 0, len = rules.length; i < len; i++) {
+        if(rules[i]===rule) {
+          index = i
+          break
+        }
+      }
+      if(index<0) return
+      parent.removeRule
+        ? parent.removeRule(index)
+        : parent.deleteRule(index)
+    })
+    delete node.omRule
+  }
+
+  var mediaStore = []
+
+  var checkMediaList = function() {
+    mediaStore.forEach(function(v) {
+      v.mediaEnable = v.mediaTest()
+      walk(v)
+    })
+  }
+
+  if(window.attachEvent) {
+    window.attachEvent('onresize', checkMediaList)
+  } else if(window.addEventListener) {
+    window.addEventListener('resize', checkMediaList, true)
+  }
+
+  var walk = function(node, opt) {
+    if (!node) return
+
+    // cssobj generate vanilla Array, it's safe to use constructor, fast
+    if (node.constructor === Array) return node.map(function (v) {walk(v, opt)})
+
+    var postArr = []
+    var children = node.children
+    var isGroup = node.type=='group'
+
+    var selText = node.selText
+    var cssText = getBodyCss(node.prop)
+
+    if(atomGroupRule(node)) opt = opt || []
+
+    if(isGroup) {
+      if(!atomGroupRule(node)){
+        var reAdd = 'omGroup' in node
+        node.omGroup = addCSSRule(sheet, sugar(node.groupText), '{}').pop()||null
+        if(node.at=='media' && !reAdd && !node.omGroup) {
+
+          var mediaTest = new Function(
+            'return ' + node.groupText
+              .replace(/@media\s*/, '')
+              .replace(/\(/g, '(document.documentElement.offsetWidth')
+              .replace(/px\s*\)/g, ')')
+              .replace(/\s+and\s+/g, '&&')
+              .replace(/,/g, '||'))
+
+          var validMediaRule = true
+          try{
+            mediaTest()
+          } catch(e) {
+            validMediaRule = false
+          }
+
+          if(validMediaRule) {
+            node.mediaTest = mediaTest
+            mediaStore.push(node)
+          }
+
+        }
+      }
+    }
+
+    var parent = getParent(node)
+
+    if (cssText) {
+      if(!atomGroupRule(node)) {
+        if(validParent(node))
+          node.omRule = addCSSRule(parent, selText, cssText, node.selPart)
+        else if(node.parentRule){
+          if(node.parentRule.mediaEnable){
+            if(!node.omRule) node.omRule = addCSSRule(parent, selText, cssText, node.selPart)
+          }else if(node.omRule) {
+            removeRule(node)
+          }
+        }
+      }
+      opt && opt.push( selText ? selText + ' {' + cssText +'}' : cssText )
+    }
+
+    for (var c in children) {
+      if(c==='' || children[c].at=='media') postArr.push(c)
+      else walk(children[c], opt)
+    }
+
+    if(isGroup) {
+      if(atomGroupRule(node) && validParent(node)) {
+        node.omRule = addCSSRule(parent, node.groupText, opt.join(''))
+        delete opt
+      }
+    }
+
+    // media rules need a stand alone block
+    postArr.map(function(v) {
+      walk(children[v], opt)
+    })
+
   }
 
   return function (result) {
-
-    var walk = function(node, opt) {
-      if (!node) return
-
-      // cssobj generate vanilla Array, it's safe to use constructor, fast
-      if (node.constructor === Array) return node.map(function (v) {walk(v, opt)})
-
-      var postArr = []
-      var children = node.children
-      var isGroup = node.type=='group'
-
-      var selText = node.selText
-      var cssText = getBodyCss(node.prop)
-
-      if(atomGroupRule(node)) opt = opt || []
-
-      if(isGroup) {
-        if(!atomGroupRule(node)) node.omGroup = addCSSRule(sheet, node.groupText, '{}').pop()||null
-      }
-
-      if (cssText) {
-        if(!atomGroupRule(node) && (!node.parentRule || node.parentRule.omGroup!==null)){
-          node.omRule = addCSSRule(getOm(node), selText, cssText, node.selPart)
-        }
-        opt && opt.push( selText ? selText + ' {' + cssText +'}' : cssText )
-      }
-
-      for (var c in children) {
-        if(c==='' || children[c].at=='@media ') postArr.push(c)
-        else walk(children[c], opt)
-      }
-
-      if(isGroup) {
-        if(atomGroupRule(node) && getOm(node)) {
-          node.omRule = addCSSRule(getOm(node), node.groupText, opt.join(''))
-          delete opt
-        }
-      }
-
-      // media rules need a stand alone block
-      postArr.map(function(v) {
-        walk(children[v], opt)
-      })
-
-    }
 
     if(!result.diff) {
       walk(result.root)
@@ -134,21 +213,7 @@ function cssobj_plugin_post_cssom (option) {
       })
 
       if(diff.removed) diff.removed.forEach(function(node) {
-        node.omRule && node.omRule.forEach(function(rule) {
-          var parent = rule.parentRule || sheet
-          var rules = parent.cssRules || parent.rules
-          var index = -1
-          for(var i = 0, len = rules.length; i < len; i++) {
-            if(rules[i]===rule) {
-              index = i
-              break
-            }
-          }
-          if(index<0) return
-          parent.removeRule
-            ? parent.removeRule(index)
-            : parent.deleteRule(index)
-        })
+        removeRule(node)
       })
 
       if(diff.changed) diff.changed.forEach(function(node) {
@@ -156,20 +221,23 @@ function cssobj_plugin_post_cssom (option) {
         var om = node.omRule
         var diff = node.diff
 
+        if(!om) return
+
+        // added have same action as changed, can be merged..
         diff.added && diff.added.forEach(function(v) {
-          om.forEach(function(rule) {
+          om && om.forEach(function(rule) {
             rule.style[v] = node.prop[v][0]
           })
         })
 
         diff.changed && diff.changed.forEach(function(v) {
-          om.forEach(function(rule) {
+          om && om.forEach(function(rule) {
             rule.style[v] = node.prop[v][0]
           })
         })
 
         diff.removed && diff.removed.forEach(function(v) {
-          om.forEach(function(rule) {
+          om && om.forEach(function(rule) {
             rule.style.removeProperty
               ? rule.style.removeProperty(v)
               : rule.style.removeAttribute(v)
@@ -178,7 +246,6 @@ function cssobj_plugin_post_cssom (option) {
 
       })
     }
-
 
     return result
   }
